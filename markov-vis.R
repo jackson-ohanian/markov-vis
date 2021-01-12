b397bd5d1d1472a96778489b61fb4168f1b262c3
Jackson Ohanian <ohanian3@illinois.edu>
2021-01-12 14:26:43 -0600

    More visualization and calculation options

--- markov-vis.R
+++ markov-vis.R
@@ -79,16 +79,42 @@ animate.markov <- function(data, itts, x0) {
     return(anim)
 }
 
-
-## DRIVER TO DEL TODO DEL
-
-df <- data.frame(c(1, 200, 0), c(1, 4, 3), c(2, 2, 4))
-df
-
-mk = to.markov(df)
-mk
-stead <- find.steady(mk, 100, c(1, 0, 0))
-plotdat <- animate.markov(mk, 100, c(1,0,0))
-animate(plotdat, renderer = gifski_renderer(), end_pause = 30)
-
-plot.steady(stead, markov=df)
+#' Visualize the steady state result
+#' @param data a markov style dataframe (NxN)
+#' @return Quick plot of the steady state
+#' @export
+visual.markov <- function(data) {
+  n <- length(data)^0.5
+  print(n)
+  x.val <- rep(0, n)
+  y.val <- rep(0, n)
+  
+  ### PLOT equidistant around a circle
+  x0 = 50
+  y0 = 50
+  r = 1
+  for (i in 0:n-1) {
+    x.val[i+1] = x0 + r * cos(2 * pi * i / n)
+    y.val[i+1] = y0 + r * sin(2 * pi * i / n)
+  }
+  
+  r.val = rep(0, n)
+  r.val[1] = 1
+  circles <- data.frame(
+    x0 = x.val,
+    y0 = y.val,
+    r = find.steady(data, 100, r.val))
+  plt <- ggplot() + coord_fixed()
+  ### Plot Circles and Labels
+  plt = plt + geom_circle(aes(x0 = x0, y0 = y0, r = r, fill = r, alpha=0.1), data = circles) + 
+    geom_label(aes(x = x0, y = y0, label = 1:n, fill = r, alpha=0.1), data = circles)
+  i <- 0:(n-1)
+  j <- 0:(n-1)
+  for (i in 1:n) {
+    for (j in 1:n) {
+      plt <- plt + geom_segment(aes(x = x.val[i], y = y.val[i], xend = x.val[j], yend = y.val[j]), inherit.aes = TRUE)
+    }
+  }
+  plt <- plt + xlab("") + ylab("") + theme(axis.ticks.y=element_blank(),axis.ticks.x=element_blank()) + ggtitle("Steady State")
+  return(plt)
+}
